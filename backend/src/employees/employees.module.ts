1d7820b0d0153180c029920c6714af1058f8ecb6
kriskilsby <151669187+kriskilsby@users.noreply.github.com>
2026-01-16 23:56:50 +0000

    Add employee endpoint to Azure backend.

--- backend/src/employees/employees.module.ts
+++ backend/src/employees/employees.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Employee } from './employee.entity';
+import { EmployeesController } from './employees.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Employee])],
+  controllers: [EmployeesController],
 })
 export class EmployeesModule {}
