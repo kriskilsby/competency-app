c59e4b7ce1880e47ad09cf316b1d4a246b29a9cc
kriskilsby <151669187+kriskilsby@users.noreply.github.com>
2026-01-17 00:30:49 +0000

    Stop DbKeepAliveService

--- backend/src/db-keep-alive.service.ts
+++ backend/src/db-keep-alive.service.ts
@@ -1,29 +1,30 @@
 // backend/src/db-keep-alive.service.ts
-import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
-import { DataSource } from 'typeorm';
+// import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+// import { DataSource } from 'typeorm';
 
-@Injectable()
-export class DbKeepAliveService implements OnModuleInit {
-  private readonly logger = new Logger(DbKeepAliveService.name);
+// ### Removed whilst I use the paid Azure tier which keeps connections alive automatically ###
+// @Injectable()
+// export class DbKeepAliveService implements OnModuleInit {
+//   private readonly logger = new Logger(DbKeepAliveService.name);
 
-  constructor(private readonly dataSource: DataSource) {}
+//   constructor(private readonly dataSource: DataSource) {}
 
-  onModuleInit() {
-    // Run immediately on startup
-    this.pingDatabase();
+//   onModuleInit() {
+//     // Run immediately on startup
+//     this.pingDatabase();
 
-    // Then every 5 minutes (300000ms)
-    setInterval(() => {
-      this.pingDatabase();
-    }, 5 * 60 * 1000);
-  }
+//     // Then every 5 minutes (300000ms)
+//     setInterval(() => {
+//       this.pingDatabase();
+//     }, 5 * 60 * 1000);
+//   }
 
-  private async pingDatabase() {
-    try {
-      await this.dataSource.query('SELECT 1');
-      this.logger.log('DB keep-alive ping successful');
-    } catch (error) {
-      this.logger.error('DB keep-alive ping failed', error);
-    }
-  }
-}
+//   private async pingDatabase() {
+//     try {
+//       await this.dataSource.query('SELECT 1');
+//       this.logger.log('DB keep-alive ping successful');
+//     } catch (error) {
+//       this.logger.error('DB keep-alive ping failed', error);
+//     }
+//   }
+// }
